3c075d595d6bb9bdf0f859543a05aa71e22bd2c1
徐志鹏 <xuzhipeng@ut.cn>
2016-06-01 17:23:02 +0800

    。

--- TestPaper/Program.cs
+++ TestPaper/Program.cs
@@ -1,14 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace TestPaper
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
             Console.WriteLine("学生甲抄的试卷：");
             TestPaper studentA = new TestPaperA();
